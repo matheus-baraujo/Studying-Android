fde405d378dde92049751cb348a51cb8314079ad
MBA <matheus-araujo1998@hotmail.com>
2020-09-05 10:11:20 -0300

    Criada uma funcao que sorteia um numero

--- app/src/main/java/com/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myfirstapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +21,17 @@ public class MainActivity extends AppCompatActivity {
         TextView texto = findViewById(R.id.textView5);
         texto.setText("Funcionou !!!!!");
     }
+
+    public void sorteio (View view) {
+
+        TextView input = findViewById(R.id.editTextTextPersonName3);
+
+    //    if (input == " ") {
+
+            int x = new Random().nextInt(11);
+            TextView texto = findViewById(R.id.textView5);
+            texto.setText("O número sorteado foi:" + x);
+
+        }
+    //}
 }
